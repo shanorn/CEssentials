32e7e5d54e1e13b8a238a700f482df86366440bb
shanorn <shanorn@gmail.com>
2022-12-06 11:11:29 +0100

    T5-Ex03

---
+++ Exercises/T5-Ex03/T5-Ex03.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/*
+Escribe una función que acepte como parámetro una letra minúscula y devuelva la
+correspondiente letra mayúscula. No podrá usarse ninguna función de las librerías
+estándar de C. El programa debe funcionar para todas las letras comprendidas entre
+la a y la z a excepción de la ñ. Ayuda: emplear la tabla de caracteres ASCII
+*/
+
+char toUpperCase (char minuscula) {
+    char mayuscula; 
+    if (minuscula >= 97 && minuscula <= 122){
+        mayuscula = minuscula - 32; 
+    } else {
+        mayuscula = 0;
+        printf("El caracter introducido no es una letra minusucla: a-z\n");
+    }
+}
+
+void main () {
+
+    char resultado = toUpperCase('a');
+    printf("%c\n", resultado);
+    resultado = toUpperCase('p');
+    printf("%c\n", resultado);
+    resultado = toUpperCase('z');
+    printf("%c\n", resultado);
+    resultado = toUpperCase('A');
+    printf("%c\n", resultado);
+    resultado = toUpperCase('.');
+    printf("%c\n", resultado);
+}
